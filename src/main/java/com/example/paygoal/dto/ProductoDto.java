23cf513305ac90889c9eb16c614fe73d8f04089b
Crxstian <cristian19overdrive@gmail.com>
2024-04-19 08:24:24 -0300

    Agreando configuracion basica swagger

--- src/main/java/com/example/paygoal/dto/ProductoDto.java
+++ src/main/java/com/example/paygoal/dto/ProductoDto.java
@@ -3,9 +3,11 @@ package com.example.paygoal.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
-public class ProductoDto {
+public class ProductoDto implements Serializable {
     private Integer id;
     private String nombre;
     private String descripcion;
